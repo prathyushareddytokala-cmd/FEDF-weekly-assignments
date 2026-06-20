16fc7b4c99522a21b23ff9f021cf2b793c43d9c8
prathyushareddytokala-cmd <prathyushareddytokala@gmail.com>
2026-06-20 08:55:39 +0530

    Add files via upload

---
+++ WEEK-5 CO5/StudentCard-Tailwind/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="bg-white p-5 rounded-lg shadow-lg text-center">
+      <h2 className="text-xl font-bold">
+        Student Profile Card
+      </h2>
+
+      <p>Name: Srihitha</p>
+      <p>Course: React JS</p>
+      <p>Batch: Section 4</p>
+
+      <span className="bg-green-500 text-white px-3 py-1 rounded">
+        Active
+      </span>
+    </div>
+  );
+}
+
+export default App;
